20284a68f33001ed5af6a951be0dbdb70a58cc39
Théo Sigaud <sigaudtheo@gmail.com>
2022-11-24 21:31:11 +0100

    Add connection with mongo

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose');
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
+
+
+let DATABASE_URL = process.env.DATABASE_URL || 'http://localhost'
+mongoose.connect(`mongodb://${DATABASE_URL}/posts`, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
 
 
 app.get('/', (req, res) => {
