3203d60230870b048171c55181458746e030747d
Théo Sigaud <sigaudtheo@gmail.com>
2022-12-04 18:30:25 +0100

    Create chatbot

---
+++ client/src/components/Chatbot.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="container vh-100">
+      <div>
+        <p>{{question}}</p>
+
+        <div v-for="answer in answers" :key="answer.id">
+          <button class="btn btn-primary mb-4" @click="nextQuestion(answer.id)">{{answer.question}}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import chatbot from '../chatbot.json';
+
+  export default {
+    name: "Chatbot",
+
+    data () {
+      return {
+        question: '',
+        answers: [],
+        pathQuestion: null
+      }
+    },
+
+    mounted() {
+      this.question = chatbot.question
+      this.answers = chatbot.answers
+      this.pathQuestion = chatbot
+    },
+
+    methods: {
+      nextQuestion(id) {
+        this.question = this.pathQuestion.answers[id].question
+        this.answers = this.pathQuestion.answers[id].answers
+        this.pathQuestion = this.pathQuestion.answers[id]
+      }
+    }
+  }
+</script>
+
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,13 +2,18 @@
     <div>
       <div class="container vh-100">
         <h1>YOOOO</h1>
+        <Chatbot />
       </div>
     </div>
 </template>
 
 <script>
+  import Chatbot from '@/components/Chatbot.vue';
   export default {
     name: "Home",
+    components: {
+      Chatbot
+    },
 
     data () {
       return {
